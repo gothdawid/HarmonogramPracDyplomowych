825daa7b2dc80b0be3cc73a71d1c446694caac38
JC-D3NT0N <milosz.mariusz.ollech@gmail.com>
2023-04-03 22:29:07 +0200

    Implemented Defense model and importing class

--- app/Models/Defense.php
+++ app/Models/Defense.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Schedule extends Model
+class Defense extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'student',
+        'promoter',
+        'examiner',
+    ];
 }
