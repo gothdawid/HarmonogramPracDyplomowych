8aadf73b76760838814cb00961e7e45f0e4ee836
gothdawid <dawidstu@gmail.com>
2023-04-05 19:54:38 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::get('/', function () {
 });
 
 
-Route::get('/schedule', 'App\Http\Controllers\ScheduleControler@index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -29,6 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/schedule', [ScheduleController::class, 'index'])->name('');
 });
 
 require __DIR__.'/auth.php';
