022fc1222374aed3c0c9fffb2d7dbdcf3fd0138e
DawidKonarczak <dawid.konarczak@ideeconcept.pl>
2023-04-06 00:00:08 +0200

    change footer data and layout

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,16 +1,15 @@
 <footer class="rounded-lg dark:bg-gray-800">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© <?= date('Y'); ?> 
+        <a href="https://github.com/Seelf" target="_blank" class="hover:underline">Dawid Konarczak</a>,
+        <a href="https://github.com/Gianna0" target="_blank" class="hover:underline">Joanna Wojtasińska</a>,
+        <a href="https://github.com/gothdawid" target="_blank" class="hover:underline">Dawid Studziński</a>,
+        <a href="https://github.com/JC-D3NT0N" target="_blank" class="hover:underline">Miłosz Ollech</a>. 
+        All Rights Reserved.
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
             <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
         </li>
         <li>
             <a href="#" class="hover:underline">Contact</a>
