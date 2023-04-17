c2ad916c972c359163ac48849a760a24eecf8ecf
Dawid Konarczak <konarczak.dawid@gmail.com>
2023-04-17 19:56:42 +0200

    fix already calendars not displaying

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -12,7 +12,17 @@
                         <i class='fas fa-calendar-alt'></i>
                         <span class="flex-1 ml-3 whitespace-nowrap">{{ __("New Calendar") }}</span>
                     </a>
+                </li>
+                
+                @foreach (Auth::user()->calendars()->get() as $calendar)
+                    <li>
+                        <a href="{{ route('calendar', $calendar->id) }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 {{ (request()->routeIs('calendar.show')) ? 'bg-indigo-600' : '' }}">
+                            <i class='fas fa-calendar-alt'></i>
+                            <span class="flex-1 ml-3 whitespace-nowrap">{{ $calendar['Calendar_Name'] }}</span>
+                        </a>
                     </li>
+                @endforeach
+
                 <li>
                     <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                         <i class='fas fa-database'></i>
