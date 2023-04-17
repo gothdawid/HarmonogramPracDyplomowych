c5042f2cf8a7528f33135c3c97fe689f41c2ce19
Dawid Konarczak <konarczak.dawid@gmail.com>
2023-04-17 20:34:54 +0200

    add logo, add limit for displaying calendars on left side (5) order by desc

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -14,7 +14,7 @@
                     </a>
                 </li>
                 
-                @foreach (Auth::user()->calendars()->get() as $calendar)
+                @foreach (Auth::user()->calendars()->orderBy('created_at', 'desc')->limit(5)->get() as $calendar)
                     <li>
                         <a href="{{ route('calendar', $calendar->id) }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 {{ (request()->routeIs('calendar.show')) ? 'bg-indigo-600' : '' }}">
                             <i class='fas fa-calendar-alt'></i>
