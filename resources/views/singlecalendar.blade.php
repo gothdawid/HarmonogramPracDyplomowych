885e62087646d3023d7ad375c54c53d239e08a98
gothdawid <dawidstu@gmail.com>
2023-04-24 17:36:09 +0200

    Calendar name in page header

--- resources/views/singlecalendar.blade.php
+++ resources/views/singlecalendar.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Single Calendar') }}
+            {{ __('Single Calendar') }} - {{ App\Models\Calendar::find($calendar_id)->Calendar_Name }}
+
         </h2>
     </x-slot>
 
